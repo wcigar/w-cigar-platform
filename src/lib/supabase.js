7d4736e513fbf9ad82429a19b351d46598c1d419
Wilson <w1063442145@gmail.com>
2026-04-07 23:24:20 +0800

    v2.0 GAS to Supabase migration

--- src/lib/supabase.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-const supabaseUrl = 'https://yzujoxdltvklrehphzsl.supabase.co'
-const supabaseAnonKey = 'sb_publishable_gtr2N8UbDtmJbTcmibKYwQ_Pl6oK-GU'
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
